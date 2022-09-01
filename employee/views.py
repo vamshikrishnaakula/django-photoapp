6181e44f9492ef656a144f3002f7df8e3c6afc0e
vamshikrushna123@gmail.com <96808439+vamshikrishnaakula@users.noreply.github.com>
2022-09-01 18:09:49 +0530

    Add files via upload

--- employee/views.py
+++ employee/views.py
@@ -2,6 +2,7 @@ from sre_constants import SUCCESS
 from django.shortcuts import render,redirect,HttpResponse
 from django.http import JsonResponse
 from .forms import NameForm
+# from django.contrib import messages
 from .models import Applicants,Imagedata, candidate_photo
 import sys
 from subprocess import run,PIPE
@@ -17,6 +18,7 @@ import base64
 import cv2
 from imutils import paths
 import cv2,os,re
+from django.template import RequestContext
 
 
 def Homepage(request):
@@ -35,18 +37,32 @@ def help(request):
 def add1(request):
         global val
         val=request.POST.get('num2')
-        
+             
+        applicant = Applicants.objects.get(pk=val)
+        print("applicant==",applicant)
+        # mydata = Applicants.objects.values_list('pk')
+        # # print("mydata===",mydata)
+        # for num in Applicants.objects.all():
+        #         print("num====",num)
+        # #   if applicant in num:
         num = 10 # define the length of the string   
-        res = ''.join(secrets.choice(string.ascii_letters + string.digits) for x in range(num))
-        global res1 
+        res = ''.join(secrets.choice( string.digits) for x in range(num)) 
         res1=res.upper() 
         print("res1==",res1)
         token = candidate_photo.objects.filter(pk=val).update(token=res1 )
         print("token===",token)
-        applicant = Applicants.objects.get(pk=val)
         candidate_data = candidate_photo.objects.get(pk=val)
-        return render(request,'working_trail.html',{'result': applicant,'result_photo': candidate_data})
-        # return render(request,'working_trail.html',{'result': applicant})
+        return render(request,'working_trail.html',{'result': applicant,'result_photo': candidate_data})   
+        # res1=res.upper() 
+        #         print("res1==",res1)
+        #         token = candidate_photo.objects.filter(pk=val).update(token=res1 )
+        #         print("token===",token)
+        #         candidate_data = candidate_photo.objects.get(pk=val)
+        #         return render(request,'working_trail.html',{'result': applicant,'result_photo': candidate_data})   
+        #             # return render(request,'working_trail.html',{'result': applicant})          
+        # # else:
+        #         print("no record found")
+        #         return render(request,"Homepage.html")
 def chipdata(request):
         # num = 10 # define the length of the string   
         # res = ''.join(secrets.choice(string.ascii_letters + string.digits) for x in range(num))
@@ -76,40 +92,49 @@ def chipdata(request):
         f0ggy=open("D:/Django-crud-application-master/images/"+ val+".jpg","wb")
         f0ggy.write(base64data)
         f0ggy.close()
-
-        # dir_path = ("D:/Django-crud-application-master/images/"+"44"+".jpg")
-        # dir_path = (base64data)
-        # dir_path=("D:/facesblur/face (3).jpg")
-        threshold = 250.0
-        #
-        # #
-        #
+        threshold = 350.0
         image = cv2.imread("D:/Django-crud-application-master/images/"+ val+".jpg")
-        # print("image type is=======",image)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        fm = variance_of_laplacian(gray)
-        print("this is the variance type=====", int(fm), fm)
-        text = "Not Blurry"
-        if fm < threshold:
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        faces = face_cascade.detectMultiScale(gray,1.3,5)
+        print("faces",faces)
+        if len(faces) !=0 :
+            for (x,y,w,h) in faces :
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                faces = image[y:y + h, x:x + w]
+                # print("faces==2",faces)
+                fm = variance_of_laplacian(faces)
+                print("this is the variance type=====", int(fm), fm)
+                text = "Not Blurry"
+
+                if fm >= threshold:
+                    print(fm)
+                    print("this is at notBlur_per=")
+                    '''save into DB'''
+                    cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+                    cv2.imshow("Image", faces)
+                    key = cv2.waitKey(0)
+                    chipdata=Applicants.objects.filter(pk=val).update(BIB=BiB,chipcode1=Chipcode1,chipcode2=Chipcode2)
+                    print("chipdata=",chipdata)
+                    candidate_image = candidate_photo.objects.filter(pk=val).update(candidate_photo=imagedata)
+                    print("candidate_image===",candidate_image)
+                    
+                    return render(request,'working_trail.html')
+                else:
                     print(fm)
                     text = "Blurry"
+                    print(" photo is not clear")
                     """take the picture again """
-                    message = " photo is not clear"
-                    return HttpResponse({"message":message})
-
+                    # mesages.error(request, 'photo is not clear')
+                    return render(request,'working_trail.html')
+                    # return redirect("add1")
         else:
-            print(fm)
-            print("this is at notBlur_per=")
-            '''save into DB'''
-            chipdata=Applicants.objects.filter(pk=val).update(BIB=BiB,chipcode1=Chipcode1,chipcode2=Chipcode2)
-            print("chipdata=",chipdata)
-            candidate_image = candidate_photo.objects.filter(pk=val).update(candidate_photo=imagedata)
-            print("candidate_image===",candidate_image)
-            cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-            cv2.imshow("Image", image)
-            key = cv2.waitKey(0)
-        return render(request,'working_trail.html')
+            print("no faces were detected")
+            return render(request,'working_trail.html')
+
+      
+            
         #  return render(request,'working_trail.html')'result_photo': candidate_data
 
           
